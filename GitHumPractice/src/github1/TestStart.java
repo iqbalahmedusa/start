4f13bfcbf6a79b47a1440955839c264fd5fd80c5
iqbal <iqbal@DESKTOP-CKDMN9M.home>
2020-04-18 21:41:41 -0400

    my first github

---
+++ GitHumPractice/src/github1/TestStart.java
@@ -0,0 +1,17 @@
+package github1;
+
+public class TestStart {
+	public void myMethod() {
+		int x= 100;
+		int y= 200;
+		int z=x+y;
+		System.out.println("addition of x and y :"+z);
+	}
+
+	public static void main(String[] args) {
+		TestStart obj = new TestStart();
+		obj.myMethod();
+
+	}
+
+}
